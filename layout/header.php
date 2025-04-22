c4390aab0a1913cbd4246679ae4a54e5c34a28a2
Lucas Conde <lucasconde742@gmail.com>
2025-04-21 23:11:30 -0300

    Editando los estilos y nombres de HEADER/NAV

---
+++ layout/header.php
@@ -0,0 +1,9 @@
+<header>
+    <div>
+        <img src="../img/logo-sin.png" alt="Logo de la barberia"  sizes="(max-width: 320px)">
+    </div>
+
+    <a href="../index.php">
+        <h1>God is Good</h1>
+    </a>
+</header>
